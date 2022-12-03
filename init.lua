e8e14206875cd49f255bdfc7779290c31fe9029c
sugarmouse <tanghao19950911@gmail.com>
2022-12-03 23:40:33 +0800

    basci config from Takuya Matsuyama

---
+++ init.lua
@@ -0,0 +1,15 @@
+require('th.base')
+require('th.highlights')
+require('th.maps')
+require('th.plugins')
+
+local has = vim.fn.has
+local is_mac = has "macunix"
+local is_win = has "win32"
+
+if is_mac then
+  require('craftzdog.macos')
+end
+if is_win then
+  require('craftzdog.windows')
+end

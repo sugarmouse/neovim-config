29e747e179fa254d6fa081173328c4ebd58abf36
sugarmouse <tanghao19950911@gmail.com>
2022-12-03 23:43:06 +0800

    fix file path problem

--- init.lua
+++ init.lua
@@ -8,8 +8,8 @@ local is_mac = has "macunix"
 local is_win = has "win32"
 
 if is_mac then
-  require('craftzdog.macos')
+  require('th.macos')
 end
 if is_win then
-  require('craftzdog.windows')
+  require('th.windows')
 end
